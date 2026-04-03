5c7c21cc65ffb88d0311fbe80f813d6d7dfb244a
sameer abbas <L1F24BSCS0315@ucp.edu.pk>
2026-04-03 23:35:47 +0500

    Add files via upload

---
+++ Queue.h
@@ -0,0 +1,90 @@
+﻿#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Queue
+{
+private:
+    T* arr;
+    int size;
+    int front;
+    int rear;
+    int count;
+
+public:
+    // Constructor: initializes queue of given size
+    Queue(int s)
+    {
+        size = s;
+        arr = new T[size];
+        front = 0;
+        rear = -1;
+        count = 0;
+
+    }
+
+    // Destructor: frees dynamically allocated memory
+    ~Queue()
+    {
+        delete[] arr;
+    }
+
+    // Enqueue: adds element to the rear of the queue
+    void Enqueue(T value)
+    {
+        if (isFull())
+        {
+            cout << "Queue is full! Cannot enqueue." << endl;
+            return;
+        }
+        rear = (rear + 1) % size;
+        arr[rear] = value;
+        count++;  // count was not decleared but was used so i decleared it and initiate it by constructor
+    }
+
+    // Dequeue: removes and returns element from the front
+    T Dequeue()// i have changed dequeue function 
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty! Cannot dequeue." << endl;
+            return T(); // return default object
+        }
+
+        T value = arr[front];
+        front++;
+        count--;
+
+        if (count == 0)   
+        {
+            front = 0;
+            rear = -1;
+        }
+
+        return value;
+    }
+
+    // Peek: returns front element without removing it
+    T Peek()
+    {
+        return arr[front];
+    }
+
+    // isEmpty: returns true if queue has no elements
+    bool isEmpty()
+    {
+        return count == 0;
+    }
+
+    // isFull: returns true if queue has reached capacity
+    bool isFull()
+    {
+        return  count==size;
+    }
+
+};
+
+#endif // QUEUE_H
